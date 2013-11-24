d5ad6f671c72630de98ba8a4f8c34d542c62ba3f
Duncan Ralph <dkralph@mit.edu>
2013-11-24 12:06:49 +0100

    rm two files

--- SiteReadiness/readi.sh
+++
@@ -1,6 +0,0 @@
-logdir=`date +%m/%d`
-webdir=$HOME/www/readiness
-mkdir -p $webdir $logdir
-#./SiteReadiness.py -p $webdir -u http://cern.ch/test-readiness -x true
-./SiteReadiness.py -p $webdir -u $HOME/www -x false
-#./UsableSites.py  -p /tmp/$USER/usable -u http://lhcweb.cern.xch/cms
