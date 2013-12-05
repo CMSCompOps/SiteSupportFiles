c26958a73ffc99bda06ea99c34404393639c3e68
Alan <alan.malta@cern.ch>
2013-12-05 15:03:16 +0100

    script now receives input and output file as arguments

--- savannah/parseSavannah.py
+++ savannah/parseSavannah.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import sys,getopt,re
+import sys,re
 import urllib
-from datetime import datetime
 import time
 import calendar
+from optparse import OptionParser
+from datetime import datetime
 from xml.dom import minidom
 
 def getText(nodelist):
@@ -19,19 +20,22 @@ def getTag(node,tagname):
 
 current = datetime.utcnow()
 current_unix = int(calendar.timegm(current.timetuple()))
-input_xml_filename = None
-opts, args = getopt.getopt(sys.argv[1:], "", ["file="])
 
-# check command line parameter
-for opt, arg in opts :
-    if opt == "--file" :
-        input_xml_filename = arg
+parser = OptionParser(add_help_option=False)
+parser.add_option("-h", "--help", action="help")
+parser.add_option("-i", "--input", dest="inFile",
+                  help="(Optional) Input XML file, under http, containing all the savannah information")
+parser.add_option("-o", "--output", dest="outFile",
+                  help="(Mandatory) HTML file containing the output of this script")
+(options, args) = parser.parse_args()
 
-if input_xml_filename == None:
+if options.outFile == None:
+    parser.error("You must provide the output file name (use -o option) or consult --help\n")
+if options.inFile == None:
     url='https://savannah.cern.ch/export/cmscompinfrasup/gutsche/526.xml'
     input = urllib.urlopen(url)
-else :
-    input = open(input_xml_filename)
+else:
+    input = urllib.urlopen(options.inFile)
 
 RE_XML_ILLEGAL = u'([\u0000-\u0008\u000b-\u000c\u000e-\u001f\ufffe-\uffff])' + \
                  u'|' + \
@@ -44,7 +48,7 @@ xmldoc = minidom.parseString(x)
 
 # for savannah in xmldoc.childNodes :
 #     for item in savannah.childNodes:
-#         print item.getElementsByTagName('item_id')[0]
+#         f.write(item.getElementsByTagName('item_id')[0]
 
 tickets = {}
 squads = {}
@@ -73,7 +77,7 @@ for item in items:
         for event in events:
             date = int(getTag(event, 'date'))
         last_modification = int((current_unix-date)/86400)
-    #print "id: %i, category: %s, squad: %s, summary: %s, last_mod: %4i days" % (id,category,squad, summary, last_modification)
+    #f.write("id: %i, category: %s, squad: %s, summary: %s, last_mod: %4i days" % (id,category,squad, summary, last_modification)
     if category not in tickets.keys(): tickets[category] = {}
     tickets[category][id] = {'summary':summary,'squad':squad,'site':site,'submitted_on':submitted_on,'open_since':open_since,'last_modified':last_modification,'ggus':ggus,'ggus_url':ggus_url}
     if squad not in squads.keys(): squads[squad] = 0
@@ -82,33 +86,36 @@ for item in items:
 categories = tickets.keys()
 categories.sort()
 
-print '<html>'
-print '<pre>'
-print '--------------------------------------------------------------------------------'
-print 'Report generated on',current,'UTC'
-print '--------------------------------------------------------------------------------'
+f = open(options.outFile, 'w')
 
-print ''
-print 'Summary: open tickets per category'
-print '--------------------------------------------------------------------------------'
-print ''
+f.write('<html>')
+f.write('<pre>')
+f.write('--------------------------------------------------------------------------------<br>')
+f.write('Report generated on %s UTC<br>' % current)
+f.write('--------------------------------------------------------------------------------<br>')
+
+f.write('<br>')
+f.write('Summary: open tickets per category<br>')
+f.write('--------------------------------------------------------------------------------<br>')
+f.write('<br>')
 for category in categories:
-    print "%40s: %5i open tickets" % (category,len(tickets[category].keys()))
+    f.write("%40s: %5i open tickets<br>" % (category,len(tickets[category].keys())))
 
-print ''
-print 'Summary: open tickets per squad'
-print '--------------------------------------------------------------------------------'
-print ''
+f.write('<br>')
+f.write('Summary: open tickets per squad<br>')
+f.write('--------------------------------------------------------------------------------<br>')
+f.write('<br>')
+
 sorted_squads = squads.keys()
 sorted_squads.sort()
 for squad in sorted_squads:
-    print "%40s: %5i open tickets" % (squad,squads[squad])
+    f.write("%40s: %5i open tickets<br>" % (squad,squads[squad]))
 
 for category in categories:
-    print ''
-    print category
-    print '--------------------------------------------------------------------------------'
-    print ''
+    f.write('<br>')
+    f.write(category)
+    f.write('<br>--------------------------------------------------------------------------------<br>')
+    f.write('<br>')
     timemap = {}
     for id in tickets[category].keys():
         if tickets[category][id]['open_since'] not in timemap.keys(): timemap[tickets[category][id]['open_since']] = []
@@ -122,8 +129,9 @@ for category in categories:
             except:
                 ggus_id = 0
             if ggus_id == 0:
-                print "<a href='http://savannah.cern.ch/support/index.php?%i'>SAV:%i</a> open since %4i days and last modified %4i days ago in squad: %35s at site: %20s and summary: %s" % (id,id,time,tickets[category][id]['last_modified'],tickets[category][id]['squad'],tickets[category][id]['site'],tickets[category][id]['summary'])
+                f.write("<a href='http://savannah.cern.ch/support/index.php?%i'>SAV:%i</a> open since %4i days and last modified %4i days ago in squad: %35s at site: %20s and summary: %s<br>" % (id,id,time,tickets[category][id]['last_modified'],tickets[category][id]['squad'],tickets[category][id]['site'],tickets[category][id]['summary']))
             else :
-                print "<a href='https://ggus.eu/ws/ticket_info.php?ticket=%i'>GGUS:%i</a> open since %4i days and last modified %4i days ago in squad: %35s at site: %20s and summary: %s" % (ggus_id,ggus_id,time,tickets[category][id]['last_modified'],tickets[category][id]['squad'],tickets[category][id]['site'],tickets[category][id]['summary'])
-print '</pre>'
-print '</html>'
+                f.write("<a href='https://ggus.eu/ws/ticket_info.php?ticket=%i'>GGUS:%i</a> open since %4i days and last modified %4i days ago in squad: %35s at site: %20s and summary: %s<br>" % (ggus_id,ggus_id,time,tickets[category][id]['last_modified'],tickets[category][id]['squad'],tickets[category][id]['site'],tickets[category][id]['summary']))
+f.write('</pre>')
+f.write('</html>')
+f.close()
