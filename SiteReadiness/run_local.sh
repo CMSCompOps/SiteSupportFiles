0d23486cc153f84dd20bb9129753102b4c409337
John P. Artieda <artiedaj@fnal.gov>
2014-03-22 12:49:06 -0500

    run_local.sh

--- SiteReadiness/run_local.sh
+++ SiteReadiness/run_local.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
 # Script to run a secondary version of the SR script when the main script fails:
 # acronjob in acrontab jartieda
-# 38 0,6,12,18 * * * lxplus ssh vocms202 /afs/cern.ch/user/j/jartieda/SiteSupportFiles/SiteReadiness/run_local.sh &> /dev/null
+# 38 0,6,12,18 * * * lxplus /afs/cern.ch/user/j/jartieda/SiteSupportFiles/SiteReadiness/run_local.sh
+# 43 * * * * lxplus ssh vocms202 cp -a $HOME/www/SR/. /afs/cern.ch/cms/LCG/www/sreadiness/SiteReadiness/
 location=$HOME/SiteSupportFiles/SiteReadiness
 webdir=$HOME/www/SR
 webofficial=/afs/cern.ch/cms/LCG/www/sreadiness/SiteReadiness
 link=http://cms-site-readiness.web.cern.ch/cms-site-readiness
 
-cd $location
-
-# fixing access
-echo "exporting KEY and CERT"
-export X509_USER_CERT=/data/certs/servicecert.pem
-export X509_USER_KEY=/data/certs/servicekey.pem
-
 # python script
+cd $location
 ./SiteReadiness.py -p $webdir -u $link
 # $webdir: output location
 # $link: address to use inside files for output links
