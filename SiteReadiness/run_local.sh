4d2446010310d97b23ea125803e1419e4aece70b
John P. Artieda <artiedaj@fnal.gov>
2014-04-23 16:17:43 -0500

    SR reports 2
    includes WR metric and Disk endpoints

--- SiteReadiness/run_local.sh
+++ SiteReadiness/run_local.sh
@@ -3,10 +3,11 @@
 # acronjob in acrontab jartieda
 # 38 0,6,12,18 * * * lxplus /afs/cern.ch/user/j/jartieda/SiteSupportFiles/SiteReadiness/run_local.sh
 # 43 * * * * lxplus ssh vocms202 cp -a $HOME/www/SR/. /afs/cern.ch/cms/LCG/www/sreadiness/SiteReadiness/
-location=$HOME/SiteSupportFiles/SiteReadiness
-webdir=$HOME/www/SR
-webofficial=/afs/cern.ch/cms/LCG/www/sreadiness/SiteReadiness
-link=http://cms-site-readiness.web.cern.ch/cms-site-readiness
+location=$HOME/SiteSupportFiles/SiteReadiness2
+webdir=$HOME/www/SR2
+link=https://jartieda.web.cern.ch/jartieda/SR2
+#webofficial=/afs/cern.ch/cms/LCG/www/sreadiness/SiteReadiness
+#link=http://cms-site-readiness.web.cern.ch/cms-site-readiness
 
 # python script
 cd $location
@@ -15,5 +16,5 @@ cd $location
 # $link: address to use inside files for output links
 
 # copy output files to web location
-cp -a $webdir/. $webofficial/
-echo "*** All copies completed ***"
+#cp -a $webdir/. $webofficial/
+#echo "*** All copies completed ***"
