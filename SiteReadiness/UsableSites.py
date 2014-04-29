4ba9ab745c3d9412ef6af71ea1f1ce65ae2dcd15
Gökhan Kandemir <gokhan.kandemir@cern.ch>
2014-04-29 17:35:35 +0200

    siteDB v2

--- SiteReadiness/UsableSites.py
+++ SiteReadiness/UsableSites.py
@@ -5,6 +5,7 @@
 import sys, xml.dom.minidom, os, datetime, time, pprint
 from xml import xpath
 from datetime import date
+import simplejson as json
 
 # OptParse
 from optparse import OptionParser
@@ -98,31 +99,33 @@ ColumnMatrix['SRM_sam']=SRM_sam
 ColumnMatrix['Ranking']=Ranking
 
 # -----------------------------------------------------------------------------------------------------------
-
-SiteDB_url="https://cmsweb.cern.ch/sitedb/reports/showXMLReport?reportid=naming_convention.ini"
+	
+#SiteDB_url="https://cmsweb.cern.ch/sitedb/reports/showXMLReport?reportid=naming_convention.ini"
+SiteDB_url="https://cmsweb.cern.ch/sitedb/data/prod/federations-sites"
 SiteDB_sites=[]
 
-fileSiteDB=pathN+"/sitedb.xml"
-
 print "Getting the url %s" % SiteDB_url
-os.system("curl -ks -H 'Accept: text/xml'  '%s' > %s" % (SiteDB_url,fileSiteDB))
+#os.system("curl -ks -H 'Accept: text/xml'  '%s' > %s" % (SiteDB_url,fileSiteDB))
+os.system("curl -ks --cert $X509_USER_PROXY --key $X509_USER_PROXY  '%s' > %s" % (SiteDB_url,fileSiteDB))
 	
 f=file(fileSiteDB,'r')
-t= xml.dom.minidom.parse(f)
+#t= xml.dom.minidom.parse(f)
+rows=json.loads(f)
 f.close()
 
-for urls in xpath.Evaluate('/report/result/item', t):
+#for urls in xpath.Evaluate('/report/result/item', t):
+#	info={}
+#	for target in xpath.Evaluate("cms", urls):
+#      		if target.hasChildNodes():
+#		      	s=target.firstChild.nodeValue.encode('ascii')
+#	       	else:
+#	      		s=""
 
-	info={}
-	for target in xpath.Evaluate("cms", urls):
-      		if target.hasChildNodes():
-		      	s=target.firstChild.nodeValue.encode('ascii')
-	       	else:
-	      		s=""
+#		if s not in SiteDB_sites:
+#			SiteDB_sites.append(s)
 
-		SiteDB_sites.append(s)
-
-#pprint.pprint(SiteDB_sites)
+for siteName in rows['result']:
+	SiteDB_sites.append(siteName[3]) 
 
 ########################################################
 # Reading data from SSB
