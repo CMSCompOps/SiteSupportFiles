f8b7ee7ecbf35a38b21aa2d0b6258f36b21140ac
John P. Artieda <artiedaj@fnal.gov>
2014-06-05 17:02:50 -0500

    adding copies to CommLinksReports web

--- SiteReadiness/run_local.sh
+++ SiteReadiness/run_local.sh
@@ -6,21 +6,26 @@
 location=$HOME/SiteSupportFiles/SiteReadiness
 webdir=$HOME/www/SR2
 webofficial=/afs/cern.ch/cms/LCG/www/sreadiness/SiteReadiness
+webofficial2=/afs/cern.ch/cms/LCG/www/sreadiness/CommLinksReports
 link=http://cms-site-readiness.web.cern.ch/cms-site-readiness
 
 # Running all necessary scripts
 cd $location
 # Active links
+echo "*** EnabledLinksFromPhEDExDataSrv.py ***"
 python EnabledLinksFromPhEDExDataSrv.py -p $webdir -u $link
 
 # Site Readiness python
+echo "*** SiteReadiness.py ***"
 ./SiteReadiness.py -p $webdir -u $link
 # $webdir: output location
 # $link: address to use inside files for output links
 
 # Usable sites for analysis
+echo "*** UsableSites.py ***"
 python UsableSites.py -p $webdir -u $link
 
 # copy all output files to web location
 cp -a $webdir/. $webofficial/
+cp -a $webdir/. $webofficial2/
 echo "*** All copies completed ***"
