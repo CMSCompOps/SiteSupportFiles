9626da8f7f9f191c3fed95c013098e2d7b33beeb
John P. Artieda <artiedaj@fnal.gov>
2014-06-05 17:48:10 -0500

    included info about acronjobs

--- SiteReadiness/run_local.sh
+++ SiteReadiness/run_local.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Script to run a secondary version of the SR script when the main script fails:
 # acronjob in acrontab jartieda
-# 38 0,6,12,18 * * * lxplus /afs/cern.ch/user/j/jartieda/SiteSupportFiles/SiteReadiness/run_local.sh
-# 43 * * * * lxplus ssh vocms202 cp -a $HOME/www/SR2/. /afs/cern.ch/cms/LCG/www/sreadiness/SiteReadiness/
+# 38 0,6,12,18 * * * lxplus /afs/cern.ch/user/j/jartieda/SiteSupportFiles/SiteReadiness/run_local.sh &> /dev/null
+# 43 * * * * lxplus ssh vocms202 cp -a $HOME/www/SR2/. /afs/cern.ch/cms/LCG/www/sreadiness/SiteReadiness/ &> /dev/null
+# 43 * * * * lxplus ssh vocms202 cp -a $HOME/www/SR2/. /afs/cern.ch/cms/LCG/www/sreadiness/CommLinksReports/ &> /dev/null
 location=$HOME/SiteSupportFiles/SiteReadiness
 webdir=$HOME/www/SR2
 webofficial=/afs/cern.ch/cms/LCG/www/sreadiness/SiteReadiness
